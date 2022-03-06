898ef19d3bb5f3321cfcd32193c65eebb4e9cd9c
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-03-05 21:08:38 -0300

    correcciohes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetchData()
+    fetchDatos() //llama a funcion para traer datos del JSON
 })
 
-const fetchData = async () => {
+const fetchDatos = async () => {//Funcion para traer platos disponibles desde JSON
     try {
         const res = await fetch('bd.json')
         const data = await res.json()
         // console.log(data)
-        platos(data)
+        mostrarPlatos(data)
         detectarBotones(data)
     } catch (error) {
         console.log(error)
@@ -15,12 +15,10 @@ const fetchData = async () => {
 }
 
 const contendorProductos = document.querySelector('#contenedor-productos')
-const platos = (data) => {
+const mostrarPlatos = (data) => { //Funcion para cargar datos del JSON en forma de tarjetas en el HTML
     const template = document.querySelector('#template-productos').content
     const fragment = document.createDocumentFragment()
-    // console.log(template)
     data.forEach(producto => {
-        // console.log(producto)
         template.querySelector('img').setAttribute('src', producto.thumbnailUrl)
         template.querySelector('h5').textContent = producto.title
         template.querySelector('p span').textContent = producto.precio
@@ -31,44 +29,41 @@ const platos = (data) => {
     contendorProductos.appendChild(fragment)
 }
 
-let carrito = {}
+let resumen = {}
 
-const detectarBotones = (data) => {
+const detectarBotones = (data) => { //escuchar botones de tarjetas para agregar el plato a la mesa
     const botones = document.querySelectorAll('.card button')
 
     botones.forEach(btn => {
         btn.addEventListener('click', () => {
-            // console.log(btn.dataset.id)
             const producto = data.find(item => item.id === parseInt(btn.dataset.id))
             producto.cantidad = 1
-            if (carrito.hasOwnProperty(producto.id)) {
-                producto.cantidad = carrito[producto.id].cantidad + 1
+            if (resumen.hasOwnProperty(producto.id)) {
+                producto.cantidad = resumen[producto.id].cantidad + 1
             }
-            carrito[producto.id] = { ...producto }
-            // console.log('carrito', carrito)
-            pintarCarrito()
+            resumen[producto.id] = { ...producto }
+            mostrarResumen()
         })
     })
 }
-//marcador representacion de resumen de mesa
+
 const items = document.querySelector('#items')
 
-const pintarCarrito = () => {
+const mostrarResumen = () => { //muestra resumen de lo cargado en la mesa
 
-    //pendiente innerHTML
     items.innerHTML = ''
 
-    const template = document.querySelector('#template-carrito').content
+    const template = document.querySelector('#template-resumen').content
     const fragment = document.createDocumentFragment()
 
-    Object.values(carrito).forEach(producto => {
-        // console.log('producto', producto)
+    Object.values(resumen).forEach(producto => {
+
         template.querySelector('th').textContent = producto.id
         template.querySelectorAll('td')[0].textContent = producto.title
         template.querySelectorAll('td')[1].textContent = producto.cantidad
         template.querySelector('span').textContent = producto.precio * producto.cantidad
         
-        //botones
+        //botones para sumar o restar items
         template.querySelector('.btn-info').dataset.id = producto.id
         template.querySelector('.btn-danger').dataset.id = producto.id
 
@@ -83,42 +78,14 @@ const pintarCarrito = () => {
 
 }
 
-
-const items2 = document.querySelector('#items2')
-
-const pintarFactura = () => {
-
-    //pendiente innerHTML
-    items2.innerHTML = ''
-
-    const template2 = document.querySelector('#template-factura').content
-    const fragment2 = document.createDocumentFragment()
-
-    Object.values(carrito).forEach(producto => {
-        // console.log('producto', producto)
-        template2.querySelector('th').textContent = producto.id
-        template2.querySelectorAll('td')[0].textContent = producto.title
-        template2.querySelectorAll('td')[1].textContent = producto.cantidad
-        template2.querySelector('span').textContent = producto.precio * producto.cantidad
-
-        const clone2 = template2.cloneNode(true)
-        fragment2.appendChild(clone2)
-    })
-
-    items2.appendChild(fragment2)
-
-    pintarFactura()
-
-}
-
-const footer = document.querySelector('#footer-carrito')
+const footer = document.querySelector('#footer-resumen')
 const pintarFooter = () => {
 
     footer.innerHTML = ''
 
-    if (Object.keys(carrito).length === 0) {
+    if (Object.keys(resumen).length === 0) {
         footer.innerHTML = `
-        <th scope="row" colspan="5">Se ha limpiado el registro de la mesa</th>
+        <th scope="row" colspan="5">Se han eliminado los pedidos de la mesa</th>
         `
         return
     }
@@ -127,9 +94,8 @@ const pintarFooter = () => {
     const fragment = document.createDocumentFragment()
 
     // sumar cantidad y sumar totales
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
-    // console.log(nPrecio)
+    const nCantidad = Object.values(resumen).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(resumen).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
 
     template.querySelectorAll('td')[0].textContent = nCantidad
     template.querySelector('span').textContent = nPrecio
@@ -140,10 +106,10 @@ const pintarFooter = () => {
     footer.appendChild(fragment)
 
 
-    const boton = document.querySelector('#vaciar-carrito')
+    const boton = document.querySelector('#vaciar-resumen')
     boton.addEventListener('click', () => {
-        carrito = {}
-        pintarCarrito()
+        resumen = {}
+        mostrarResumen()
     })
 
 }
@@ -152,37 +118,27 @@ const accionBotones = () => {
     const botonesAgregar = document.querySelectorAll('#items .btn-info')
     const botonesEliminar = document.querySelectorAll('#items .btn-danger')
 
-    // console.log(botonesAgregar)
 
     botonesAgregar.forEach(btn => {
         btn.addEventListener('click', () => {
-            // console.log(btn.dataset.id)
-            const producto = carrito[btn.dataset.id]
+            const producto = resumen[btn.dataset.id]
             producto.cantidad ++
-            carrito[btn.dataset.id] = { ...producto }
-            pintarCarrito()
+            resumen[btn.dataset.id] = { ...producto }
+            mostrarResumen()
         })
     })
 
     botonesEliminar.forEach(btn => {
         btn.addEventListener('click', () => {
-            // console.log('eliminando...')
-            const producto = carrito[btn.dataset.id]
+
+            const producto = resumen[btn.dataset.id]
             producto.cantidad--
             if (producto.cantidad === 0) {
-                delete carrito[btn.dataset.id]
+                delete resumen[btn.dataset.id]
             } else {
-                carrito[btn.dataset.id] = { ...producto }
+                resumen[btn.dataset.id] = { ...producto }
             }
-            pintarCarrito()
+            mostrarResumen()
         })
     })
 }
-
-// let carritoEjemplo = {}
-// carritoEjemplo = {
-//     1: {id: 1, titulo: 'cafe', precio: 500, cantidad: 3},
-//     2: {id: 3, titulo: 'pizza', precio: 100, cantidad: 2},
-// }
-
-// console.log(carritoEjemplo[1])
